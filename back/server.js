a799d27385171dacc0e0a9f53b62a1b88309a9fe
codavidev <male.brut@gmail.com>
2022-03-22 07:59:24 +0100

    install node, mongoose, express, nodemon - basic  server started

---
+++ back/server.js
@@ -0,0 +1,10 @@
+console.log("HELLO WORLD");
+console.log("NODEMON OK");
+
+const http = require('http');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
